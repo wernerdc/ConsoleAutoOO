f44d4ac0b4b9869fdfe30e03316846060adff1b9
wernerdc <manuel.werner3@gmx.de>
2024-08-02 21:37:40 +0200

    Projektdateien hinzufügen.

---
+++ ConsoleAutoOO/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleAutoOO {
+    internal class Program {
+        static void Main(string[] args) {
+            bool appRunning = true;
+            while (appRunning) {
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Clear();
+                Console.WriteLine("ConsoleAutoOO \n");
+
+
+
+
+                Console.Write("\n\nProgramm beenden (e)? ");
+                try {
+                    string exitApp = Console.ReadLine();
+                    if (exitApp.ToUpper() == "E") {
+                        appRunning = false;
+                    }
+                } catch {
+                    // no error message, just keep going and repeat the app
+                }
+            }
+        }
+    }
+}
