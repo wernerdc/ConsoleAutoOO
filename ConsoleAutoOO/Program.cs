a67600af92b40524559ad846d94bb9879187b3f0
wernerdc <manuel.werner3@gmx.de>
2024-08-21 11:12:15 +0200

    Auto Liste + Ausgabe als Tabelle

--- ConsoleAutoOO/Program.cs
+++ ConsoleAutoOO/Program.cs
@@ -11,22 +11,58 @@
                 Console.Clear();
                 Console.WriteLine("ConsoleAutoOO \n");
 
-                Auto auto1 = new Auto();
-                Console.WriteLine("auto1 parameterloser Konstruktor: \n" + auto1.ToString());
+                List<Auto> autos = [new Auto("BMW", "Z8", 4941, 400, 250, 500),
+                                    new Auto("Alfa Romeo", "147", 1910, 150, 208, 305),
+                                    new Auto("VW", "Golf VIII", 1968, 116, 202, 300),
+                                    new Auto("Honda", "Civic XI", 1993, 143, 180, 186)];
 
-                auto1.Hersteller = "Alfa Romeo";
-                auto1.Bezeichnung = "147";
-                auto1.Hubraum = 1900;
-                auto1.PS = 150;
-                auto1.Geschwindigkeit = 215;
-                auto1.Drehmoment = 2000;
+                //for (int i = 0; i < autos.Count; i++) {
+                //    Console.WriteLine(autos[i].ToString());
+                //    Console.WriteLine();
+                //}
 
-                Console.WriteLine("\nauto1:");
-                Console.WriteLine(auto1.ToString());
+                //Console.WriteLine("--------------------------------");
+                //foreach (Auto auto in autos) {
+                //    Console.WriteLine(auto.ToString());
+                //    Console.WriteLine();
+                //}
+                //Console.WriteLine("--------------------------------");
 
-                Auto auto2 = new Auto("BMW", "Dicke Karre", 4500, 390, 290, 6000);
-                Console.WriteLine("\nauto2:");
-                Console.WriteLine(auto2.ToString());
+                Console.WriteLine("{0,-16} {1,-16} {2,8} {3,6} {4,6} {5,6}",
+                        "Hersteller",
+                        "Modell",
+                        "ccm",
+                        "ps",
+                        "km/h",
+                        "nm");
+
+                for (int i = 0; i < autos.Count; i++) {
+                    //Console.WriteLine($"{autos[i].Hersteller,-10} {autos[i].Bezeichnung,-10} {autos[i].Hubraum,6} {autos[i].PS,4} {autos[i].Geschwindigkeit,4} {autos[i].Drehmoment,4}");
+                    Console.WriteLine("{0,-16} {1,-16} {2,8} {3,6} {4,6} {5,6}", 
+                            autos[i].Hersteller, 
+                            autos[i].Bezeichnung, 
+                            autos[i].Hubraum, 
+                            autos[i].PS, 
+                            autos[i].Geschwindigkeit, 
+                            autos[i].Drehmoment);
+                }
+
+                //Auto auto1 = new Auto();
+                //Console.WriteLine("auto1 parameterloser Konstruktor: \n" + auto1.ToString());
+
+                //auto1.Hersteller = "Alfa Romeo";
+                //auto1.Bezeichnung = "147";
+                //auto1.Hubraum = 1900;
+                //auto1.PS = 150;
+                //auto1.Geschwindigkeit = 215;
+                //auto1.Drehmoment = 2000;
+
+                //Console.WriteLine("\nauto1:");
+                //Console.WriteLine(auto1.ToString());
+
+                //Auto auto2 = new Auto("BMW", "Dicke Karre", 4500, 390, 290, 6000);
+                //Console.WriteLine("\nauto2:");
+                //Console.WriteLine(auto2.ToString());
 
                 Console.Write("\n\nProgramm beenden (e)? ");
                 try {
